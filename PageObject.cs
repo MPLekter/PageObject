563c19fbbcfa978b25ed3985201be9cb14ecf57a
Michał Paulo <65944671+MPLekter@users.noreply.github.com>
2023-01-22 13:04:56 +0100

    PageObject is the base class. SteamPage is the example child.

---
+++ PageObject.cs
@@ -0,0 +1,55 @@
+﻿using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Here all common actions that can be used on any page are stored.
+//TODO: think about refactoring this so it can be inherited by any PageObject? 
+namespace Task2._1
+{
+    public class PageObject
+    {
+        private static IWebDriver driver;
+        private Actions actions = new Actions(Singleton.Driver());
+
+        public PageObject(IWebDriver Driver)
+        {
+            driver = Driver;
+        }
+
+        public string GetCurrentURL()
+        {
+            return driver.Url;
+        }
+        public string GetCurrentWindowHandle()
+        {
+            return driver.CurrentWindowHandle;
+        }
+        public void MoveToElement(IWebElement element)
+        {
+            actions.MoveToElement(element).Perform();
+        }
+
+        public void ClickElement(IWebElement element)
+        {
+            element.Click();
+        }
+
+        public void ChangeWindow(string originalWindow)
+        {
+            foreach (string window in driver.WindowHandles)
+            {
+                if (originalWindow != window)
+                {
+                    driver.SwitchTo().Window(window);
+                    break;
+                }
+            }
+        }
+
+    }
+}
+
---
+++ SteamPage.cs
@@ -0,0 +1,22 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2._1
+{
+    public class SteamPage : PageObject
+    {
+        private static IWebDriver driver;
+        public IWebElement privacyPolicyButton = Singleton.Driver().FindElement(By.XPath("//*[@id=\"footer_text\"]/div[2]/a[1]"));
+
+        public SteamPage(IWebDriver Driver) : base(Driver)
+        {
+            driver = Driver;
+        }
+    }
+}
